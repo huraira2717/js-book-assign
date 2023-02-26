c31234c504f59a614e99468466f4d02266bc310e
Huraira2717 <122014990+Huraira2717@users.noreply.github.com>
2023-02-26 22:56:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,222 @@
+// var Array=[]
+// console.log(Array)
+
+// var stdArr=["","","","","","",""]
+// console.log(stdArr)
+
+// var string=["mango","Apple","banana"]
+// console.log(typeof(string))
+
+// var number = [1,2,3,4,5,6,7,8,9,10]
+// console.log(number)
+
+// var boolArr=[true,false,true,false];
+// console.log(boolArr[0]);
+// console.log(boolArr[2])
+
+
+// var mixArr=["MAngo","Apple",10,20];
+// console.log(mixArr)
+
+// document.write("<h1>Qualiffcation:</h1>")
+// var listArr=["SSC","HSC","BSC","BS","BCOM","MS","M.Phil","phD"];
+// console.log(listArr.length)
+// document.write("4)",listArr[0]);document.write("<br/>")
+// document.write("5)",listArr[1]);document.write("<br/>")
+// document.write("6)",listArr[2]);document.write("<br/>")
+// document.write("4)",listArr[3]);document.write("<br/>")
+// document.write("5)",listArr[4]);document.write("<br/>")
+// document.write("6)",listArr[5]);document.write("<br/>")
+// document.write("7)",listArr[6]);document.write("<br/>")
+// document.write("8)",listArr[7])
+
+
+// var nameArr=["Michael is .","Jhon is .","Tony is ."];
+// var scoreArr=[320,230,480];
+// var perArr=[500]
+// console.log(nameArr[0]+perArr/scoreArr[0]);
+// console.log(nameArr[1]+perArr/scoreArr[1]);
+// console.log(nameArr[2]+perArr/scoreArr[2])
+
+
+// document.write(nameArr[0]);
+// document.write(scoreArr[0]);
+// document.write(perArr*scoreArr/1000);
+// document.write("percentage:")
+// document.write("<br />");
+
+// document.write(nameArr[1]);
+// document.write(scoreArr[1]);
+// document.write(nameArr[1]+scoreArr/perArr[1])
+// document.write("percentage:")
+// document.write("<br />");
+
+// document.write(nameArr[2]);
+// document.write(scoreArr[2]);
+// document.write(nameArr[2]+scoreArr/perArr[2])
+// document.write("percentage:")
+
+
+// var stdArr=[320,230,480,120]
+// console.log(stdArr.sort())
+
+
+// var cities=["karachi","lahore","islamabad","quetta","peshawar"]
+// console.log(cities)
+// var selected=cities.slice(2,4);
+// console.log("selected",selected)
+
+
+// var arr=["This","is","my","cat"]
+// var str=arr.join("");
+// console.log(str)
+
+// var arr=[];
+
+// arr.push("first");
+// arr.push("second");
+// arr.push("third");
+
+// var firstvalue=arr.shift();
+// console.log("firstvalue:",firstvalue);
+// console.log("remaining value",arr.join(","));
+
+
+// var arr=[];
+
+// arr.push("first");
+// arr.push("second");
+// arr.push("third");
+
+// var lastvalue=arr.pop();
+// console.log("lastvalue:",lastvalue);
+// console.log("remaining value",arr.join(","));
+
+
+// var manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+// document.write("<select>");
+// for (var i = 0; i < manufacturers.length; i++) {
+//   document.write("<option>" + manufacturers[i] + "</option>");
+// }
+// document.write("</select>");
+
+
+// var arr=[
+//     [],
+//     [],
+//     []
+// ];
+// console.log(arr)
+
+// var a=[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// console.log(a)
+
+
+
+// for(var i =1; i <=10;++i)
+// document.write(i + "<br />" )
+
+
+// var tableNumber = +prompt("Enter Table number");
+// var tablelength = +prompt("Enter table length");
+
+// for (var i = 1; i <= tablelength; i++) {
+//     document.write(tableNumber + " x" + " " + i + " " + " = " + " " + tableNumber * i + "<br />")
+// }
+
+    
+//      var stdArr = ["Apple", "MAngo", "Banana", "Orange", "strawberry"]
+//      console.log(stdArr.length)
+//      console.log(stdArr[0])
+    
+//      document.write("<h1>" + stdArr[0] + "</h1>")
+    
+    
+//      for (var i = 0; i < stdArr.length; i++) {
+//          console.log(i, "index number")
+//          document.write("<h1>" + stdArr[i] + "</h1>")
+//     }
+
+
+// for (var i = 1; i <= 10; i++) {
+//           console.log(i)
+//       }
+
+
+
+// for (var i = 10; i > 0; i--) {
+//           document.write(i + "<br />")
+     
+//       }
+
+// for(var i =0; i<=20; ++i){
+//    if(i % 2 == 0){
+//    console.log("even number",i);
+//    }
+// }
+
+// for(var i =0; i <=100; ++i){
+//    if(i % 2 !==0){
+//       console.log("odd Number",i)
+//    }
+// }
+
+
+
+// for(var i =0; i <=20; ++i){
+//    if(i % 2 == 0){
+//       console.log("Serise",i )
+//    }
+// }
+
+
+//  var bakeryitem=["apple","mango","banana"];
+
+//  var userinput=prompt("Welcom to the ABC bakery.what do you want to order?");
+//  console.log("userinput",userinput);
+
+//  var isMatch=false
+
+//  for(var i =0; i <bakeryitem.length;i++){
+//     if(bakeryitem[i]===userinput){
+//         isMatch=true
+//         document.write(userinput + " " + "is avaiable at index " + i + " in our bakery <br />");
+//     break;
+//     }
+//  }if(isMatch===false){
+//     alert("We are sorry");
+//  }
+ 
+
+// var num = [24,53,78,91,12]
+// var temp = 0
+
+// for(var i = 0; i < num.length ; i++){
+// if( temp < num[i]){
+// temp=num[i]
+
+// }
+// }
+// console.log("the largest number is :",temp)
+
+// var num=[24,53,78,91,12];
+// var temp = 0;
+
+// for(var i=0; i < num.length ; i--){
+//     if(temp < num [i]){
+//         temp=num[i]
+//     }
+// }
+// console.log(temp)
+
+// for(var i = 0 ; i <=100 ; ++i){
+//     if(i % 5 == 0){
+//         console.log("That is",i)
+//     }
+// }
+
+    
